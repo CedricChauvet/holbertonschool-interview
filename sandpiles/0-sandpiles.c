f9502274b5113f981102d3195fca0db064cc26f5
CedricChauvet <chauvetc.ujf@gmail.com>
2024-05-30 14:54:39 +0200

    sandpiles C99 error

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -25,7 +25,8 @@ static void print_grid(int grid[3][3])
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int grid_unstable[3][3];
-	int is_unstable = 0;
+	int is_unstable;
+	is_unstable = 0;
 	int i, j;
 
 	for (i = 0; i < 3; i++)
