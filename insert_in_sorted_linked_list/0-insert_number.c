20ee75903c3a4e601ef849a710d78425a1ceeb3a
CedricChauvet <chauvetc.ujf@gmail.com>
2024-08-30 16:15:34 +0200

    try1 bis

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -3,33 +3,40 @@
 #include <stdio.h>
 #include "lists.h"
 
+
 listint_t *insert_node(listint_t **head, int number)
 {
-    // Allouer de la mémoire pour le nouveau nœud
-    listint_t *new_node = malloc(sizeof(listint_t));
-    
-    if (new_node == NULL) {
-        return NULL; // Gérer l'erreur d'allocation
-    }
-    new_node->n = number;
-    new_node->next = NULL;
+/**
+ * insert_node - insert a number into a sorted singly linked list
+ * @head: pointer to head of list
+ * @number: number to be included in new node
+ * Return: address of the new element or NULL if it fails
+ */
+	/* Allouer de la mémoire pour le nouveau neud */
+	listint_t *new_node = malloc(sizeof(listint_t));
 
-    // Si la liste est vide ou si le nombre est inférieur au premier élément
-    if (*head == NULL || number < (*head)->n) {
-        new_node->next = *head;
-        *head = new_node;
-        return new_node;
-    }
+	new_node->n = number;
+	new_node->next = NULL;
 
-    // Trouver la position d'insertion
-    listint_t *current = *head;
-    while (current->next != NULL && current->next->n < number) {
-        current = current->next;
-    }
+    /*Si la liste est vide ou si le nombre est inf au 1er élément */
+	if (*head == NULL || number < (*head)->n)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 
-    // Insérer le nouveau nœud à la position donnée ci dessus
-    new_node->next = current->next;
-    current->next = new_node;
+	/* Trouver la position d'insertion */
+	listint_t *current = *head;
 
-    return (new_node);
+	while (current->next != NULL && current->next->n < number)
+	{
+		current = current->next;
+	}
+
+	/* Insérer le nouveau nœud à la position donnée ci dessus */
+	new_node->next = current->next;
+	current->next = new_node;
+
+	return (new_node);
 }
