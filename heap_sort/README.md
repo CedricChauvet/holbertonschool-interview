9c7e4cc49549fff033bfe51a932b2a0acbd1b9b4
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-05 10:36:53 +0200

    configure

---
+++ heap_sort/README.md
@@ -0,0 +1,1 @@
+"heap sorting in C" 
---
+++ heap_sort/sort.h
@@ -0,0 +1,8 @@
+#include <stddef.h>
+// sort.h
+#ifndef SORT_H
+#define SORT_H
+
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+#endif // SORT_H
