0d8a48cb87f9afa87afa3b149cda6d777f4aeffc
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-05 10:41:06 +0200

    configure

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -7,5 +7,10 @@
 void heap_sort(int *array, size_t size){
     // Build the heap
     print_array( array, size);
-
+    
+    // Open a file in write mode
+    FILE *fptr;
+    fptr = fopen("0-O", "w");
+    fprintf(fptr, "Hello, World!\n");
+    fclose(fptr);
     }
