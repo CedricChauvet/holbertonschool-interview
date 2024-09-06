d038edc2bb0089d9ee59d346595d1053def8102d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-06 11:29:06 +0200

    fixing complexity

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -119,3 +119,20 @@ void heap_sort(int a[], int count)
 		siftDown(a, 0, end, count);
 	}
 }
+
+
+void write_big_o_notations()
+{
+    FILE *file = fopen("0-O", "w");
+    if (file == NULL)
+    {
+        perror("Failed to open file");
+        return;
+    }
+
+    fprintf(file, "O(n log n)\n");   // Best case time complexity
+    fprintf(file, "O(n log n)\n"); // Average case time complexity
+    fprintf(file, "O(n log n)\n");   // Worst case time complexity
+	fprintf(file, "\n");
+    fclose(file);
+}
