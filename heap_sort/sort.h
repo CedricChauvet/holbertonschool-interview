96713f8f81757aa085cc7392abde97db3635a5f6
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-06 11:40:22 +0200

    fixing complexity

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -1,9 +1,12 @@
-#include <stddef.h>
-// sort.h
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+
 void print_array(const int *array, size_t size);
 void heap_sort(int arr[], int n);
-void siftDown(int arr[], int n, int i, int len);
-#endif // SORT_H
+void siftDown(int arr[], int n, int i, int length);
+void heapify(int a[], int count);
+void swap(int *a, int *b);
+void write_big_o_notations();
+#endif
