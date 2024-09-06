815e509053f892b598b73e3f18c6890f3d5c0857
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-06 10:42:39 +0200

    fixing betty

--- heap_sort/0-heap_sort.c
+++ heap_sort/0-heap_sort.c
@@ -3,46 +3,67 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/*This function swaps the values of two integers.*/
+void swap(int *a, int *b)
 
-
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
-void siftDown(int a[], int start, int end, int length) {
-    int root = start;
-    while (2 * root + 1 < end) {
-        int child = 2 * root + 1;
-        if (child + 1 < end && a[child] < a[child + 1]) {
-            child++;
-        }
-        if (a[root] < a[child]) {
-            swap(&a[root], &a[child]);
-            print_array(a, length); // Utilisation de la taille du tableau
-            root = child;
-        } else {
-            return;
-        }
-    }
+
+/*This sorts.*/
+void siftDown(int a[], int start, int end, int length)
+
+{
+	int root = start;
+
+	while (2 * root + 1 < end)
+	{
+		int child = 2 * root + 1;
+
+		if (child + 1 < end && a[child] < a[child + 1])
+		{
+			child++;
+		}
+
+		if (a[root] < a[child])
+		{
+			swap(&a[root], &a[child]);
+			print_array(a, length);
+			root = child;
+		}
+		else
+		{
+			return;
+		}
+	}
 }
 
-void heapify(int a[], int count) {
-    int start = (count / 2) - 1;
-    while (start >= 0) {
-        siftDown(a, start, count, count);
-        start--;
-    }
+void heapify(int a[], int count)
+/*This sorts.*/
+{
+	int start = (count / 2) - 1;
+
+	while (start >= 0)
+	{
+		siftDown(a, start, count, count);
+		start--;
+	}
 }
 
-void heap_sort(int a[], int count) {
-    heapify(a, count);
-    int end = count;
-    while (end > 1) {
-        end--;
-        swap(&a[0], &a[end]);
-        print_array(a, count); // Utilisation de la taille du tableau
-        siftDown(a, 0, end, count);
-    }
+void heap_sort(int a[], int count)
+/*This sorts.*/
+{
+	heapify(a, count);
+	int end = count;
+
+	while (end > 1)
+	{
+		end--;
+		swap(&a[0], &a[end]);
+		print_array(a, count);
+		siftDown(a, 0, end, count);
+	}
 }
