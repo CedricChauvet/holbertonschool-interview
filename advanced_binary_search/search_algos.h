8f8d364b723ff008da538d7138e8bb478a1e92b1
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-13 17:34:27 +0200

    dernier effort, :(

--- advanced_binary_search/search_algos.h
+++ advanced_binary_search/search_algos.h
@@ -1,6 +1,11 @@
+#ifndef ADVANCED_BINARY_H
+#define ADVANCED_BINARY_H
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
 int advanced_binary(int *array, size_t size, int value);
 void print_array(const int *array, size_t size);
+
+#endif /* ADVANCED_BINARY_H */
