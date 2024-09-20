7ea5669061f502caf1464274a4f965e507d5cc08
CedricChauvet <chauvetc.ujf@gmail.com>
2024-09-20 17:50:46 +0200

    my way

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """
-make a List of keywords
+2eme version
+use a recursive function
+
+
 """
 from functools import reduce
 import re
@@ -15,27 +18,25 @@ def count_words(subreddit, word_list):
 
     if 'data' in data and 'children' in data['data']:
         titres = list(map(lambda article: article.get('data', {}).get('title', ''), data['data']['children']))
-        result = reduce(lambda x, y: x + ' ' + y, titres).lower()
+        result = " ".join(titres).lower()
         word_list = sorted(set(map(str.lower, word_list)))
         
         # Appel initial à la fonction récursive
-        count_words_recursive(word_list, result)
-    else:
-        print("La structure de la réponse n'est pas celle attendue.")
+        parcourir_liste_recursif(word_list, result)
 
 
-def count_words_recursive(word_list, result):
-    # Cas de base : si la liste de mots est vide, on arrête la récursion
-    if not word_list:
+
+
+
+def parcourir_liste_recursif(liste, result):
+    # Condition d'arrêt : Si la liste est vide, on arrête
+    if not liste:
         return
+    liste[0] = liste[0].lower()
+    pattern = r'\b' + liste[0]+ r'\b' #expression regex pour le mot complet
+    count_python =  len(re.findall(pattern, result)) #utilisation de re
+    #count_python = result.count(liste[0])
+    print(f"{liste[0]}: {count_python}")  
     
-    # Traiter le premier mot de la liste
-    mot = word_list[0]
-    pattern = r'\b' + re.escape(mot) + r'\b'
-    count = len(re.findall(pattern, result))
-    print(f"{mot}: {count}")
-    
-    # Appel récursif avec le reste de la liste
-    count_words_recursive(word_list[1:], result)
-
-
+    # Appeler la fonction récursive sur le reste de la liste (sauf le premier élément)
+    parcourir_liste_recursif(liste[1:],result)
