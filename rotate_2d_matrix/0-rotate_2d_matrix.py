8f7b584772d0230f927004a4b70253da8bf8a06e
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-04 15:27:32 +0200

    isokay

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,17 +3,14 @@
 Test  - Rotate 2D Matrix
 """
 
-# A = [[1, 2, 3],
-#      [4, 5, 6],
-#      [7, 8, 9]]
 
 def rotate_2d_matrix(matrix):
     """
     this one rotates a 2D matrix 90 degrees clockwise
     """
+
     A = []
     for i in range(len(matrix)):
         A .append(list(reversed([matrix[j][i] for j in range(len(matrix))])))
 
     matrix[:] = A
-         
