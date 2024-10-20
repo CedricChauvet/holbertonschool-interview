239ec014a52e73bb9db92f9f3d4ea61ed80baaf1
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-20 09:51:56 +0200

    try3

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -5,21 +5,14 @@ called Minimum operations
 by Ced
 """
 
+
 def minOperations(n):
-    if n <= 1:
-        return 0
-    
-    operations = 0
-    clipboard = 1
-    current = 1
-    
-    while current < n:
-        if n - current >= current:
-            clipboard = current
-            current += clipboard
-            operations += 2
+    sol = 0
+    while n > 1:
+        if n%2 == 0:
+            sol += 2
+            n = n//2
         else:
-            current += clipboard
-            operations += 1
-    
-    return operations
+            sol += 1
+            n = n - 1
+    return sol 
