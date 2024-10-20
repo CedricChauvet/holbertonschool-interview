3e6c0782588878a07b06974c51c98fd9cc8107e1
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-20 10:22:31 +0200

    with comments

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -6,14 +6,18 @@ int is_palindrome(listint_t **head){
     listint_t *current = *head;
     int i = 0, j = 0, k = 0;
     int arr[10000];
+    // this create arr of linked list
     while (current != NULL){
         arr[i] = current->n;
         current = current->next;
         i++;
     }
+
+    //compare array with reverse array
     j = i - 1;
     while (k < j){
         if (arr[k] != arr[j]){
+            // if not palindrome
             return 0;
         }
         k++;
