ec477c66b936474c5958d65cf7c64d1e212c6dfd
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-20 09:37:47 +0200

    task0 try0

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -7,4 +7,12 @@ by Ced
 
 
 def minOperations(n):
-    pass
+    sol = 0
+    while n > 1:
+        if n%2 == 0:
+            sol += 2
+            n = n/2
+        else:
+            sol += 1
+            n = n - 1
+    return sol 
