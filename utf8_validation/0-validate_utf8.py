89727c5b55b5358d15111f38232c6c861462178d
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-23 15:07:48 +0200

    tyr2

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -5,7 +5,12 @@ Main file for testing
 
 
 def validUTF8(data):
+    """
+    test si une liste de caracter est un utf8
+    """
     for i in data:
-        if i >255:
+        if (i >0x7f and i < 0XC280) or i > 0XC3BF:
             return False
     return True
+
+# 	C280 c3 bf
