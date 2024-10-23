cd96588f1e03bbd08b7fc7e26e0d7ccf134d4d1c
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-23 14:51:11 +0200

    try4

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -6,10 +6,10 @@
 
 int is_palindrome(unsigned long number)
 {
-    // if(number == ULONG_MAX)
-    // {
-    //     return (0);
-    // }
+    // Gestion de ULONG_MAX
+    if (number == ULONG_MAX)
+        return 0;  // ULONG_MAX n'est pas un palindrome
+        
 
     int c = 0; /* digit position */
     long long n; /* number */
