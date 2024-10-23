b3e4d59c8d3330bd41a01712e4a7f0d047c03cc5
CedricChauvet <chauvetc.ujf@gmail.com>
2024-10-23 14:52:05 +0200

    try5 missing 1 check

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -7,8 +7,8 @@
 int is_palindrome(unsigned long number)
 {
     // Gestion de ULONG_MAX
-    if (number == ULONG_MAX)
-        return 0;  // ULONG_MAX n'est pas un palindrome
+    // if (number == ULONG_MAX)
+    //     return 0;  // ULONG_MAX n'est pas un palindrome
         
 
     int c = 0; /* digit position */
