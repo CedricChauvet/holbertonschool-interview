480108a7fca1bc9136cb476369172781e9fec5e8
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-12 16:14:12 +0100

    try 6

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -8,28 +8,30 @@ def validUTF8(data):
     """
     test si une liste de caracter est un utf8
     """
+
     n_bytes = 0
     
-    # Pour chaque entier dans la donnée.
+    # Pour chaque entier dans la donnée
     for num in data:
-        # Récupère le masque binaire de l'octet le plus à gauche.
+        # Récupère le masque binaire de l'octet le plus à gauche
         bin_rep = format(num, '#010b')[-8:]
         
-        # Si c'est un octet de la suite.
+        # Si c'est un octet de suite
         if n_bytes != 0:
-            if bin_rep[0:2] != '10':
+            # Vérifie si le masque binaire commence par 10
+            if not (bin_rep[0:2] == '10'):
                 return False
             n_bytes -= 1
             continue
         
-        # Sinon, détermine le nombre d'octets sur la base du masque.
+        # Sinon, détermine le nombre d'octets sur la base du masque
         elif bin_rep[0] == '0': n_bytes = 0
         elif bin_rep[0:3] == '110': n_bytes = 1
         elif bin_rep[0:4] == '1110': n_bytes = 2
         elif bin_rep[0:5] == '11110': n_bytes = 3
         else: return False
     
-    # S'il reste des octets en attente, c'est invalid.
+    # S'il reste des octets en attente, c'est invalid
     return True
 
 
