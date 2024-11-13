badebcd3b0dec6f1e816d830999ca864370e8cb5
CedricChauvet <chauvetc.ujf@gmail.com>
2024-11-13 09:55:09 +0100

    its okay

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -6,18 +6,25 @@ Valid UTF-8
 
 def validUTF8(data):
     """
-    test si une liste de caracter est un utf8
+    Teste si une liste d'integer est un utf8.
+    Pour n octets, on cree une boucle for
+    sur laquelle on itere data pour verifier si
+    chaque entier est cohérent
+    
     """
 
-    n_bytes = 0
+    # on cree un iterateur pour parcourir la liste
     data = iter(data)
 
     # Pour chaque entier dans la donnée
     for num in data:
         # Récupère le masque binaire de l'octet le plus à gauche
         bin_rep = format(num, '#010b')[-8:]
+        
+        # cas ascii
         if bin_rep[0] == '0':
             continue
+
         elif bin_rep[0:3] == '110':
             # print("2 bytes")
             try:
@@ -27,6 +34,7 @@ def validUTF8(data):
                     return False
             except StopIteration:
                 return False
+        
         elif bin_rep[0:4] == '1110':
             # print("3 bytes")
             for i in range(2):
@@ -37,6 +45,7 @@ def validUTF8(data):
                         return False
                 except StopIteration:
                     return False
+        
         elif bin_rep[0:5] == '11110':
             # print("3 bytes")
             for i in range(3):
