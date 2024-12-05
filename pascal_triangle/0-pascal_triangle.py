520341a7146e7c60eba942ceff9935e8262af954
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-05 11:39:38 +0100

    pycode

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,25 +3,30 @@
 triangle de pascal
 """
 
+
 def pascal_triangle(n):
     """
     This is how to print the triangle of Pascal
     """
-    
+
+    # if value error
     if n <= 0:
         return []
+
+    # triangle is the returned output
     triangle = []
+
+    # first line
     former_line = [1]
+
     for i in range(1, n+1):
-        
+
         line = [0] * i
         line[0] = 1
         line[-1] = 1
-        
+
         for j in range(1, len(line)-1):
-            line[j] =  former_line[j] + former_line[j-1]
+            line[j] = former_line[j] + former_line[j-1]
         former_line = line
         triangle.append(line)
     return triangle
-        
-    
