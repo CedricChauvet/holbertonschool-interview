770d8b95f9acfd5989478fc1b1a3c09c9cec7c7b
CedricChauvet <chauvetc.ujf@gmail.com>
2024-12-12 11:59:54 +0100

    regex betty

--- regex/regex.c
+++ regex/regex.c
@@ -3,33 +3,40 @@
 
 #include "regex.h"
 
-int regex_match(const char *str, const char *pattern) {
-    // Cas de base : si le pattern est vide, le texte doit aussi être vide
-    if (*pattern == '\0') {
-        return *str == '\0';
-    }
+int regex_match(const char *str, const char *pattern)
+{
+	/*if pattern is empty, return 1 if str is empty, 0 otherwise*/
+		if (*pattern == '\0')
+	{
+		return (*str == '\0');
+	}
 
-    // Gestion du quantificateur *
-    if (*(pattern + 1) == '*') {
-        // Première boucle : zéro occurrence du caractère
-        if (regex_match(str, pattern + 2)) {
-            return 1;
-        }
+    /* Gestion du quantificateur */
+	if (*(pattern + 1) == '*')
+	{
+		/* first loop : zéro occurrence du caractère */
+		if (regex_match(str, pattern + 2))
+		{
+			return (1);
+		}
 
-        // Deuxième boucle : une ou plusieurs occurrences
-        while (*str && (*str == *pattern || *pattern == '.')) {
-            str++;
-            if (regex_match(str, pattern + 2)) {
-                return 1;
-            }
-        }
-        return 0;
-    }
+		/* second loop : one or many occurrences */
+		while (*str && (*str == *pattern || *pattern == '.'))
+		{
+			str++;
+			if (regex_match(str, pattern + 2))
+			{
+				return (1);
+			}
+		}
+		return (0);
+	}
 
-    // Cas sans quantificateur
-    if (*str && (*pattern == '.' || *pattern == *str)) {
-        return regex_match(str + 1, pattern + 1);
-    }
+	/* general case*/
+	if (*str && (*pattern == '.' || *pattern == *str))
+	{
+		return (regex_match(str + 1, pattern + 1));
+	}
 
-    return 0;
+	return (0);
 }
